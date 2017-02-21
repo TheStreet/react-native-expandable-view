4dfeefb18a5f106369b0dd9001beebfe6c48f63f
Philip Mortiboy <philip.mortiboy@thestreet.com>
2017-02-21 10:15:29 +0000

    Improved button layout

--- src/style.js
+++ src/style.js
@@ -2,9 +2,14 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
     button: {
+        position: 'absolute',
         bottom: 0,
+        left:0,
+        right:0,
         backgroundColor: '#fff',
+        flexDirection: 'row',
         justifyContent: 'center',
+        alignItems: 'center',
     },
     buttonText: {
         position: 'relative',
